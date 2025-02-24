af0cea58b00a9ceed9bfda58f315002c846e9af9
Marcos Monjes <marcos.monjes@itscordoba.edu.ar>
2025-02-24 18:45:42 -0300

    Se actualizan archivos para utilizar la imagen de docker de la app

--- db.js
+++ db.js
@@ -1,53 +1,26 @@
 const mysql = require('mysql2');
 
-function handleConnection() {
+const connectWithRetry = () => {
     const db = mysql.createConnection({
-        host: 'localhost',
+        host: 'mysql',
         user: 'root',
         password: '1234',
         database: 'cruddb2'
     });
 
-    db.connect(function(err) {
-        if(err) {
+    db.connect(err => {
+        if (err) {
             console.log('Error al conectar a la BD:', err);
-            setTimeout(handleConnection, 2000);
-            return;
-        }
-        console.log('Conectado a MySQL!');
-        
-        //crear tablas una vez que la conexión sea exitosa
-        const createTableSQLoficina = `
-            CREATE TABLE IF NOT EXISTS oficina (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                denominacion VARCHAR(255)            
-            )
-        `;
-        
-        const createTableSQLpersona = `
-            CREATE TABLE IF NOT EXISTS persona (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                nombre VARCHAR(255),
-                email VARCHAR(255),
-                oficina_id INT,
-                FOREIGN KEY (oficina_id) REFERENCES oficina(id)
-            )
-        `;
-        
-        db.query(createTableSQLoficina);
-        db.query(createTableSQLpersona);
-    });
-
-    db.on('error', function(err) {
-        console.log('Error en la BD:', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleConnection();
+            console.log('Reintentando en 5 segundos...');
+            setTimeout(connectWithRetry, 5000);
         } else {
-            throw err;
+            console.log('Conectado a la BD MySQL');
         }
     });
 
     return db;
-}
+};
 
-module.exports = handleConnection();
+const db = connectWithRetry();
+
+module.exports = db;
